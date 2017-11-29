cf5cc6f0c40a022da97dafd35809b0edab1d2444
WandZhong <robcoder@hotmail.com>
2017-11-29 21:31:18 +0100

    db model: fix the tranches check variable name

--- sql/migrations/001_initial.sql
+++ sql/migrations/001_initial.sql
@@ -14,7 +14,7 @@ CREATE TABLE tranches (
 	created_on       TIMESTAMP WITH TIME ZONE NOT NULL,
 	starts_on        TIMESTAMP WITH TIME ZONE NOT NULL,
 	executes_on      TIMESTAMP WITH TIME ZONE NOT NULL,
-	ends_on          TIMESTAMP WITH TIME ZONE CHECK (ends_ON > starts_on),
+	ends_on          TIMESTAMP WITH TIME ZONE CHECK (ends_on > starts_on),
 	supply           DECIMAL NOT NULL CHECK (supply > 0),  -- in wad
 	owner            TEXT,
 
