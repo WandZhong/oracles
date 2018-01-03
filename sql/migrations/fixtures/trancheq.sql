7fdb7fdb35f127346b22b827783bb3bc34ad2462
WandZhong <robcoder@hotmail.com>
2018-01-03 17:49:01 +0100

    remove sql
    The SQL setup and migrations code are moved to model repository

--- sql/migrations/fixtures/trancheq.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tokens VALUES
-  ('SWC', '2017-11-01', '', NULL),
-  ('MET', now(), '', NULL);
-
-INSERT INTO tranches VALUES
-	('1', 'tge1', 'SWC', now(), '2017-12-01', '2017-12-14', now() + '20 days', 100000000000000000000000, 'Scott', 10000000000000000000000);
-
-INSERT INTO tranche_prices VALUES
-	('1', 'USD', 2.4);
